d8919c629573a423f77da83df222c5f9d318692e
CoreyD19 <90002076+CoreyD19@users.noreply.github.com>
2021-09-22 20:58:18 -0400

    Add files via upload

--- Q3.cpp
+++ Q3.cpp
@@ -5,15 +5,7 @@ using namespace std;
 void findMinMax(int n, int arr[]){
 
     vector<int> max, min; // declare vectors for maximums and minimums
-  
-  
-    if (arr[0] > arr[1]) 
-        max.push_back(0); 
-                            // check first number for min or max
-    else if (arr[0] < arr[1]) 
-        min.push_back(0); 
-  
-    
+
     for(int i = 1; i < n - 1; i++) 
     { 
           
@@ -25,32 +17,24 @@ void findMinMax(int n, int arr[]){
                 (arr[i] > arr[i + 1])) 
         max.push_back(i); // if maximum add to max vector
     } 
-  
-     
-    if (arr[n - 1] > arr[n - 2])  // checking the last point of n-1 to the 
-        max.push_back(n - 1);     // second to last n-2 for a maximum
-  
-    else if (arr[n - 1] < arr[n - 2])  // checking if it is a minimin
-        min.push_back(n - 1); 
-  
-     
+
     if (max.size() > 0) 
     { 
-        cout << "Indexes of local maximums are : "; // print local maximums
+        std::cout << "\n" "Indexes of local maximums are : "; // print local maximums
         for(int a : max) 
-        cout << a+1 << " " "\n"; // ignore zero
+        cout << a+1 << " "; // ignore boundary
     } 
     else
-    cout << "There are no indexes of local maximums " << "\n"; // if no maximums
+    std::cout << "There are no indexes of local maximums " << "\n"; // if no maximums
   
     if (min.size() > 0) 
     { 
-        cout << "Indexes of local minimums are : "; 
+        std::cout << "\n" "Indexes of local minimums are : "; 
         for(int a : min) 
-        cout << a+1 << " " "\n"; // ignore zero 
+        cout << a+1 << " "; // ignore boundary 
     } 
     else
-    cout << "There are no indexes of local minimums " << "\n"; // if no minimums
+    std::cout << "There are no indexes of local minimums " << "\n"; // if no minimums
 } 
 
 
@@ -66,4 +50,5 @@ int main(){
   findMinMax(n,arr); // call function
    return 0; 
 }
+
 
